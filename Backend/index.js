6b3e69b019059ae6db92204fc6f3dac00ad2ce40
Ayush Yadav <aayush0029@gmail.com>
2023-10-23 14:00:06 +0530

    Port Fixes

--- Backend/index.js
+++ Backend/index.js
@@ -2,7 +2,7 @@ const express = require('express')
 const connectToDatabase = require('./db.js')
 const cors = require('cors')
 const app = express()
-const port = 5000
+const port = process.env.PORT || 5000
 connectToDatabase();
 
 app.use(cors());
