5a894a236681d2d7c4921ba40070a22a2c902300
Ayush Yadav <aayush0029@gmail.com>
2023-10-26 16:04:26 +0530

    Fixed Warning Component

--- New-Frontend/shantivideo/src/Components/Timeline.js
+++ New-Frontend/shantivideo/src/Components/Timeline.js
@@ -1,4 +1,4 @@
-import React , {useContext, useEffect, useState} from 'react'
+import React, { useContext, useEffect, useState } from 'react'
 import '../Styles/Warning.css'
 import AlertContext from '../Context/Alerts/AlertContext';
 import Warning from './Warning';
@@ -6,27 +6,30 @@ import { useSearchParams } from 'react-router-dom';
 
 export default function Timeline() {
 
-  const [contextVisibility , setContentVisibility] = useState(true);
+  const [contextVisibility, setContentVisibility] = useState(true);
 
   const alertt = useContext(AlertContext);
 
-  const checkAuthToken = ()=>{
-      if(localStorage.getItem('auth-token')){
-          setContentVisibility(false);
-      }
-      else{
-          alertt.updateAlert('danger','Please Login First!');
-          setContentVisibility(true)
-      }
+  const checkAuthToken = () => {
+    if (localStorage.getItem('auth-token')) {
+      setContentVisibility(false);
+    }
+    else {
+      alertt.updateAlert('danger', 'Please Login First!');
+      setContentVisibility(true)
+    }
   }
 
-  useEffect(()=>{
-      checkAuthToken();
-  },[])
+  useEffect(() => {
+    checkAuthToken();
+  }, [])
 
   return (
     <div>
-        {contextVisibility && <Warning/>}
+      {contextVisibility && <Warning />}
+      {!contextVisibility && <div className="container">
+          
+      </div>}
     </div>
   )
 }
